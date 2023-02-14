81e1eb87000b61638e841037f80fe3d382b73a3f
zoel-a <zulmyazhary32@gmail.com>
2023-02-14 18:23:06 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="grid place-items-center min-h-screen">
+      <h1 className="text-sky-600 text-3xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
